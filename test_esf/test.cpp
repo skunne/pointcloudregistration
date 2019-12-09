3bd6d515b4ae33ddc014cce221c0c564921f4db5
skunne <stephan.kunne@gmail.com>
2019-12-09 16:02:25 +0100

    successfully print matrix of esf distances between the five point clouds

--- test_esf/test.cpp
+++ test_esf/test.cpp
@@ -8,6 +8,8 @@
 
 #define HISTOGRAM_SIZE  (640)
 
+typedef pcl::ESFEstimation<PointT, pcl::ESFSignature640> ESFMaker;
+
 float esfDistance(float const *a, float const *b)
 {
   float d = 0.0;
@@ -21,10 +23,10 @@ float esfDistance(float const *a, float const *b)
 
 int main(int argc, char **argv)
 {
-  if (argc < 3)
+  if (argc < 2)
     return printUsage(argv[0]);
 
-  Params paramsalpha(argv[1]);
+/*  Params paramsalpha(argv[1]);
   Params paramsbeta(argv[2]);
   if (paramsalpha.error || paramsbeta.error)
     return (1);
@@ -46,15 +48,70 @@ int main(int argc, char **argv)
   esf_calculator.setInputCloud(cloudbeta);
   esf_calculator.compute(esfbeta);
 
-  /*pcl::visualization::PCLHistogramVisualizer visu;
-  visu.addFeatureHistogram(esfalpha, 640, "alpha");
-  visu.spin();
-  visu.addFeatureHistogram(esfbeta, 640, "beta");
-  visu.spin();*/
+  //pcl::visualization::PCLHistogramVisualizer visu;
+  //visu.addFeatureHistogram(esfalpha, 640, "alpha");
+  //visu.spin();
+  //visu.addFeatureHistogram(esfbeta, 640, "beta");
+  //visu.spin();
 
   float distance = esfDistance(esfalpha.points[0].histogram, esfbeta.points[0].histogram);
 
   pcl::console::print_highlight("Distance between the two clouds in ESF space: \n    %f\n", distance);
+
+  */
+
+  pcl::console::print_highlight("\n\n=========\n\n");
+
+  //int nbFiles = argc - 1;
+
+  std::vector<Params *> params;
+  std::vector<PointCloudT::Ptr> cloud;
+  std::vector<ESFMaker::PointCloudOut::Ptr> esfout;
+  ESFMaker esf_calculator;
+
+  for (int i = 0; i < argc - 1; ++i)
+  {
+    //pcl::console::print_highlight("print 100\n");
+    params.push_back(new Params(argv[i + 1]));
+    //pcl::console::print_highlight("print 200\n");
+    if (params[i]->error)
+      return (params[i]->error);
+    //pcl::console::print_highlight("print 300\n");
+    cloud.push_back(boost::shared_ptr<PointCloudT>(new PointCloudT));
+    //pcl::console::print_highlight("print 350\n");
+    int error_loading_file = cpr_loadFile(params[i]->filename.c_str(), params[i]->is_pcd, cloud[i]);
+    //pcl::console::print_highlight("print 400\n");
+    if (error_loading_file)
+      return (error_loading_file);
+    //pcl::console::print_highlight("print 500\n");
+    esf_calculator.setInputCloud(cloud[i]);
+    //pcl::console::print_highlight("print 600\n");
+    esfout.push_back(boost::shared_ptr<ESFMaker::PointCloudOut>(new (ESFMaker::PointCloudOut)));
+    //pcl::console::print_highlight("print 650\n");
+    esf_calculator.compute(*esfout[i]);
+    //pcl::console::print_highlight("print 700\n");
+  }
+
+  //pcl::console::print_highlight("print 800\n");
+
+  Eigen::MatrixXd dist_mat(argc - 1, argc - 1);
+  for (int i = 0; i < argc - 1; ++i)
+  {
+    for (int j = 0; j < argc - 1; ++j)
+    {
+      dist_mat(i, j) = esfDistance(esfout[i]->points[0].histogram, esfout[j]->points[0].histogram);
+    }
+  }
+
+  //pcl::console::print_highlight("print 900\n");
+  std::cout << std::endl << std::endl;
+  for (int i = 0; i < argc - 1; ++i)
+    std::cout << i << ".  " << params[i]->filename << std::endl;
+
+  std::cout << std::endl << std::endl;
+  std::cout << dist_mat;
+  std::cout << std::endl << std::endl;
+
 
   return (0);
 }
