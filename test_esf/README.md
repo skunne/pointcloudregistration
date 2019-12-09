0789b71cbface420c572fc48cc377ab93a1c9871
skunne <stephan.kunne@gmail.com>
2019-12-09 12:29:35 +0100

    loading files and readme

---
+++ test_esf/README.md
@@ -0,0 +1,19 @@
+## Testing ESF descriptors
+Compare two point clouds using ESF descriptors.
+
+### Install
+
+~~~~
+cmake .
+make
+~~~~
+
+### Usage
+Give metadata files as commandline arguments.
+
+If you use the metadata files from `../metadata`, then it is important to run binary from `../`
+
+~~~~
+cd ..
+./test_esf/test_esf metadata/big1.meta metadata/rot.meta
+~~~~
--- test_esf/test.cpp
+++ test_esf/test.cpp
@@ -1,3 +1,4 @@
+
 #include <pcl/features/esf.h>
 #include "cpr_main.h"
 #include "cpr_params.h"
@@ -10,12 +11,25 @@ int main(int argc, char **argv)
   if (argc < 3)
     return printUsage(argv[0]);
 
-  PointCloudT::Ptr cloudalpha;
-  PointCloudT::Ptr cloudbeta;
+  Params paramsalpha(argv[1]);
+  Params paramsbeta(argv[2]);
+  if (paramsalpha.error || paramsbeta.error)
+    return (1);
+
+  pcl::console::print_info("Loaded: meta files.\n");
+
+  PointCloudT::Ptr cloudalpha(new PointCloudT);
+  PointCloudT::Ptr cloudbeta(new PointCloudT);
   pcl::ESFEstimation<PointT, pcl::ESFSignature640> esf_calculator;
   typename pcl::ESFEstimation<PointT, pcl::ESFSignature640>::PointCloudOut esfalpha;
   typename pcl::ESFEstimation<PointT, pcl::ESFSignature640>::PointCloudOut esfbeta;
 
+  int error_loading_file = cpr_loadFile(paramsalpha.filename.c_str(), paramsalpha.is_pcd, cloudalpha);
+  error_loading_file = error_loading_file || cpr_loadFile(paramsbeta.filename.c_str(), paramsbeta.is_pcd, cloudbeta);
+  if (error_loading_file)
+    return (1);
 
+  pcl::console::print_info("Loaded: point cloud files.\n");
 
+  return (0);
 }
