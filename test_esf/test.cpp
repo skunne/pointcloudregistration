a987481d448995f9242e2b18797f0ae686975810
skunne <stephan.kunne@gmail.com>
2019-12-09 11:16:32 +0100

    fix CMakeLists, declare main variables for point clouds and esf computation

--- test_esf/test.cpp
+++ test_esf/test.cpp
@@ -1,10 +1,21 @@
 #include <pcl/features/esf.h>
+#include "cpr_main.h"
 #include "cpr_params.h"
 #include "cpr_loadfiles.h"
 
 #define HISTOGRAM_SIZE  (640)
 
-int main(void)
+int main(int argc, char **argv)
 {
+  if (argc < 3)
+    return printUsage(argv[0]);
+
+  PointCloudT::Ptr cloudalpha;
+  PointCloudT::Ptr cloudbeta;
+  pcl::ESFEstimation<PointT, pcl::ESFSignature640> esf_calculator;
+  typename pcl::ESFEstimation<PointT, pcl::ESFSignature640>::PointCloudOut esfalpha;
+  typename pcl::ESFEstimation<PointT, pcl::ESFSignature640>::PointCloudOut esfbeta;
+
+
 
 }
