af08f1dccf2a6c9e51673c92f7b76555ec668cb1
skunne <stephan.kunne@gmail.com>
2019-12-13 15:03:20 +0100

    fix snowman

--- scripts_python/makeSnowman.py
+++ scripts_python/makeSnowman.py
@@ -36,17 +36,19 @@ def addPointsOnSegment(ax,ay,az,bx,by,bz, n, pointcloud):
 
 def drawSnowman():
     pointcloud = []
-    addPointsOnSphere(0,0,0, 40, 16000, pointcloud)        # abdomen
-    addPointsOnSphere(0,0,40+40+20, 20, 4000, pointcloud)  # torso
-    addPointsOnSphere(0,0,80+40+10, 10, 1000, pointcloud)  # head
+    addPointsOnSphere(0,0,40, 40, 1600, pointcloud)        # abdomen
+    addPointsOnSphere(0,0,40+40+20, 20, 400, pointcloud)  # torso
+    addPointsOnSphere(0,0,80+40+10, 10, 100, pointcloud)  # head
     (thetaEye, phiEye) = (0.26, math.pi / 4)
     (xEye,yEye,zEye) = (10*math.sin(phiEye)*math.cos(-thetaEye),10*math.sin(phiEye)*math.sin(-thetaEye),120+10*math.cos(phiEye))
     addPointsOnSphere(xEye, -yEye, zEye, 2, 20, pointcloud)         # left eye
     addPointsOnSphere(xEye, yEye, zEye, 2, 20, pointcloud)          # right eye
-    addPointsOnHorizontalCone(-8,0,120, 2, -16, 100, pointcloud)    # nose
-    addPointsOnSegment(0,0,100, 0,50,100, 100, pointcloud)          # left arm
-    addPointsOnSegment(0,0,100, 0,-50,100, 100, pointcloud)         # right arm
+    addPointsOnHorizontalCone(-8,0,120, 2, -16, 25, pointcloud)    # nose
+    addPointsOnSegment(0,0,100, 0,50,100, 25, pointcloud)          # left arm
+    addPointsOnSegment(0,0,100, 0,-50,100, 25, pointcloud)         # right arm
     return pointcloud
+
+nbPointsTotal = 1600 + 400 + 100 + 20 + 20 + 25 + 25 + 25
 
 def printToPcd(outf, pointcloud):
     outf.write("VERSION 0.7\n")
@@ -54,18 +56,18 @@ def printToPcd(outf, pointcloud):
     outf.write("SIZE 4 4 4 4\n")
     outf.write("TYPE F F F U\n")
     outf.write("COUNT 1 1 1 1\n")
-    outf.write("WIDTH 21340\n")
+    outf.write("WIDTH {:d}\n".format(nbPointsTotal))
     outf.write("HEIGHT 1\n")
     outf.write("VIEWPOINT 0 0 0 1 0 0 0\n")
-    outf.write("POINTS 21340\n")
+    outf.write("POINTS {:d}\n".format(nbPointsTotal))
     outf.write("DATA ascii\n")
     for p in pointcloud:
         outf.write('{:.2f} {:.2f} {:.2f} 4294967295\n'.format(p[0], p[1], p[2]))
 
 def printToMeta(outf):
     outf.write('filename pointclouds/snowman.pcd\n')
-    outf.write('voxel_resolution 1.0\n')
-    outf.write('seed_resolution 1.5\n')
+    outf.write('voxel_resolution 10.0\n')
+    outf.write('seed_resolution 15\n')
     outf.write('color_importance 0.2\n')
     outf.write('spatial_importance 1.0\n')
     outf.write('normal_importance 1.0\n')
