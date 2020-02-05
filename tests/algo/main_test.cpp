71a2b32f01d00429aaecde552704a8df8ae1109c
skunne <stephan.kunne@gmail.com>
2020-02-05 12:19:20 +0100

    makefile to run tests automatically

--- tests/algo/main_test.cpp
+++ tests/algo/main_test.cpp
@@ -19,24 +19,17 @@ int main(int argc, char ** argv)
 
   std::vector<char const *> names;// = { "two nodes", "houses", "5 vs 6", "multiple optimal", "pointclouds" };
   std::vector<double> diff_with_human;//(5);
+  //
+  // names.push_back("two nodes");
+  // diff_with_human.push_back(test_two_twonodes_graphs());
 
-  names.push_back("two nodes");
-  diff_with_human.push_back(test_two_twonodes_graphs());
-
-  names.push_back("houses");
-  diff_with_human.push_back(test_two_house_graphs());
-
-  names.push_back("5 vs 6");
-  diff_with_human.push_back(test_5nodes_with_6nodes());
-
-  names.push_back("multiple optimal");
-  diff_with_human.push_back(test_multiple_optimal_solutions());
-
-  int ac = 3;
-  char const *av[3] = {"./tests/frankwolfe/test_frankwolfe", "metadata/big1.meta", "metadata/big3.meta"};
-  names.push_back("pointclouds");
-  diff_with_human.push_back(test_with_pointclouds(ac,av));
+  // int ac = 3;
+  // char const *av[3] = {"./tests/frankwolfe/test_frankwolfe", "metadata/big1.meta", "metadata/big3.meta"};
+  // names.push_back("pointclouds");
+  // diff_with_human.push_back(test_with_pointclouds(ac,av));
 //  diff_with_human.push_back(test_with_pointclouds(argc, argv));
+
+
 
   std::cout << std::endl << std::endl << "Tests passed:" << std::endl;
   for (std::size_t i = 0; i < diff_with_human.size(); ++i)
