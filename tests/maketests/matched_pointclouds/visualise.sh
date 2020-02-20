ac5514d74b3f6aea3160bc2ccedf66e2d923afe9
skunne <stephan.kunne@gmail.com>
2020-02-20 17:24:08 +0100

    paranoia: replace rm -f with rm -f -- in all Makefiles

--- tests/maketests/matched_pointclouds/visualise.sh
+++ tests/maketests/matched_pointclouds/visualise.sh
@@ -1,4 +1,4 @@
 sed -e 's/,/ /g' $1 > tmp.txt
 cut -d' ' -f1 --complement tmp.txt > tmp2.txt
 python3 ../../../scripts_python/other/show_pcd_file.py tmp2.txt figure.png
-rm -f tmp.txt tmp2.txt
+rm -f -- tmp.txt tmp2.txt
