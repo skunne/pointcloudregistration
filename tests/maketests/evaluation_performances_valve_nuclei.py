8ac57e4134a7944d1d8a3b50472cd87bdc2d0a21
skunne <stephan.kunne@gmail.com>
2020-10-07 18:43:52 +0200

    redo evaluation on nuclei, compare with icpransac; apparently there is a bug

--- tests/maketests/evaluation_performances_valve_nuclei.py
+++ tests/maketests/evaluation_performances_valve_nuclei.py
@@ -25,7 +25,7 @@ def calc_dist(mfile, srcfile, dstfile):
         list_of_d = distances_list_matched_pointclouds_csv.main(['./distances_list_matched_pointclouds_csv.py', mfile, srcfile, dstfile])
     avg_d = sum(list_of_d) / len(list_of_d)
     rmse = math.sqrt(sum(x*x for x in list_of_d) / len(list_of_d))
-    return avg_d, list_of_d
+    return avg_d, rmse, list_of_d
 
 prefix = ['nuclei', 'valve']
 cropped = ['', '_cropped']
@@ -38,6 +38,7 @@ suffix = '.csv'
 for p in prefix:
     x = []
     y_ouralgo = []
+    y_ouralgo_inliers = []
     y_icpransac = []
     for c in cropped:
         for t in thinned:
@@ -45,16 +46,32 @@ for p in prefix:
                 matrix_computed = 'transforms/'+p+c+t+r+infix+'_computed.csv'
                 source = 'matched_pointclouds/'+p+c+t+r+infix+'_src.csv'
                 dest = 'matched_pointclouds/'+p+c+t+r+infix+'_dst.csv'
-                avg_d, cloud_d = calc_dist(matrix_computed, source, dest)
+                avg_d, rmse, cloud_d = calc_dist(matrix_computed, source, dest)
                 x.append(c+t+r)
                 y_ouralgo.append(avg_d)
 
+                matrix_computed = 'transforms/'+p+c+t+r+infix+'_inliers_computed.csv'
+                source = 'matched_pointclouds/'+p+c+t+r+infix+'_inliers_src.csv'
+                dest = 'matched_pointclouds/'+p+c+t+r+infix+'_inliers_dst.csv'
+                avg_d, rmse, cloud_d = calc_dist(matrix_computed, source, dest)
+                y_ouralgo_inliers.append(avg_d)
+
+                matrix_icpransac = 'transforms/'+p+c+t+r+infix+'_icpransac.csv'
+                source = 'matched_pointclouds/'+p+infix+'_src.csv'
+                dest = 'matched_pointclouds/'+p+infix+'_dst.csv'
+                avg_d, rmse, cloud_d = calc_dist(matrix_icpransac, source, dest)
+                y_icpransac.append(avg_d)
+
     plt.scatter(x, y_ouralgo, label='our algo on {}'.format(p))
-    plt.scatter([], [], label='icp/ransac on {}'.format(p))
+    plt.plot(x, y_ouralgo)
+    plt.scatter(x, y_ouralgo_inliers, label='our algo inliers on {}'.format(p))
+    plt.plot(x, y_ouralgo_inliers)
+    plt.scatter(x, y_icpransac, label='icp/ransac on {}'.format(p))
+    plt.plot(x, y_icpransac)
 
     plt.legend(loc='best')
     plt.xlabel(p)
-    plt.ylabel('average distance')
+    plt.ylabel('average distance')#'rmse'
     plt.gcf().set_size_inches(16, 12)
     plt.savefig('evaluation_performances_{}.png'.format(p), dpi=100)
     #plt.ylim(0,10)
