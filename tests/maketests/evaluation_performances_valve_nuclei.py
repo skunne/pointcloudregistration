1947e1ccb4a955c16dd5f34b4cd9e01274594ebe
skunne <stephan.kunne@gmail.com>
2020-10-08 12:26:23 +0200

    big fix in logic of evaluation, small fix in Makefile

--- tests/maketests/evaluation_performances_valve_nuclei.py
+++ tests/maketests/evaluation_performances_valve_nuclei.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.insert(1, '../../scripts_python')
 
-import distances_list_matched_pointclouds_csv
+import distances_list_matched_pointclouds_pcd
 import matplotlib.pyplot as plt
 import math  # sqrt to calculate rmse
 
@@ -20,9 +20,9 @@ import math  # sqrt to calculate rmse
 
 def calc_dist(mfile, srcfile, dstfile):
     if mfile is None:
-        list_of_d = distances_list_matched_pointclouds_csv.main(['./distances_list_matched_pointclouds_csv.py', srcfile, dstfile])
+        list_of_d = distances_list_matched_pointclouds_pcd.main(['./distances_list_matched_pointclouds_csv.py', srcfile, dstfile])
     else:
-        list_of_d = distances_list_matched_pointclouds_csv.main(['./distances_list_matched_pointclouds_csv.py', mfile, srcfile, dstfile])
+        list_of_d = distances_list_matched_pointclouds_pcd.main(['./distances_list_matched_pointclouds_csv.py', mfile, srcfile, dstfile])
     avg_d = sum(list_of_d) / len(list_of_d)
     rmse = math.sqrt(sum(x*x for x in list_of_d) / len(list_of_d))
     return avg_d, rmse, list_of_d
@@ -40,31 +40,31 @@ for p in prefix:
     y_ouralgo = []
     y_ouralgo_inliers = []
     y_icpransac = []
-    for c in cropped:
+    for r in rotated:
         for t in thinned:
-            for r in rotated:
-                matrix_computed = 'transforms/'+p+c+t+r+infix+'_computed.csv'
-                source = 'matched_pointclouds/'+p+c+t+r+infix+'_src.csv'
-                dest = 'matched_pointclouds/'+p+c+t+r+infix+'_dst.csv'
-                avg_d, rmse, cloud_d = calc_dist(matrix_computed, source, dest)
+            for c in cropped:
                 x.append(c+t+r)
-                y_ouralgo.append(avg_d)
+                # matrix_computed = 'transforms/'+p+c+t+r+'.pcd_computed.csv'
+                # source = 'pointclouds/'+ p + c + t +'.pcd'
+                # dest   = 'pointclouds/'+ p + c + t + r +'.pcd'
+                # avg_d, rmse, cloud_d = calc_dist(matrix_computed, source, dest)
+                # y_ouralgo.append(avg_d)
 
-                matrix_computed = 'transforms/'+p+c+t+r+infix+'_inliers_computed.csv'
-                source = 'matched_pointclouds/'+p+c+t+r+infix+'_inliers_src.csv'
-                dest = 'matched_pointclouds/'+p+c+t+r+infix+'_inliers_dst.csv'
+                matrix_computed = 'transforms/'+p+c+t+r+'.pcd_inliers_computed.csv'
+                source = 'pointclouds/'+p+c+t+'.pcd'
+                dest   = 'pointclouds/'+p+c+t+r+'.pcd'
                 avg_d, rmse, cloud_d = calc_dist(matrix_computed, source, dest)
                 y_ouralgo_inliers.append(avg_d)
 
-                matrix_icpransac = 'transforms/'+p+c+t+r+infix+'_icpransac.csv'
-                source = 'matched_pointclouds/'+p+infix+'_src.csv'
-                dest = 'matched_pointclouds/'+p+infix+'_dst.csv'
+                matrix_icpransac = 'transforms/'+p+c+t+r+'.pcd_icpransac.csv'
+                source = 'pointclouds/'+p+c+t+'.pcd'
+                dest   = 'pointclouds/'+p+c+t+r+'.pcd'
                 avg_d, rmse, cloud_d = calc_dist(matrix_icpransac, source, dest)
                 y_icpransac.append(avg_d)
 
-    plt.scatter(x, y_ouralgo, label='our algo on {}'.format(p))
-    plt.plot(x, y_ouralgo)
-    plt.scatter(x, y_ouralgo_inliers, label='our algo inliers on {}'.format(p))
+    # plt.scatter(x, y_ouralgo, label='our algo without ransac on {}'.format(p))
+    # plt.plot(x, y_ouralgo)
+    plt.scatter(x, y_ouralgo_inliers, label='our algo on {}'.format(p))
     plt.plot(x, y_ouralgo_inliers)
     plt.scatter(x, y_icpransac, label='icp/ransac on {}'.format(p))
     plt.plot(x, y_icpransac)
