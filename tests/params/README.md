68edf5837383d40335249276fd3bb63f94e64b42
Stephan KUNNE <skunne@login-01.compute.bird2.prive>
2022-06-16 12:24:39 +0200

    complete readme for computeall*.sh commands on bird, and fix make_cmd for bird

--- tests/params/README.md
+++ tests/params/README.md
@@ -7,11 +7,27 @@ Compile :
 
 Run all computations :
 
+    cd heart/
     ./computeallmatchings.sh
     ./computealltransforms.sh
     ./computealldistances.sh
 
 Commands in those three .sh files have been generated using python script make_cmd.py
+
+Or smaller test run:
+    ./test_params heart/pointclouds/heart.meta heart/pointclouds/heart.pcd 30 150 heart/pointclouds/heart.pcd 30 150
+
+    java -jar ../maketests/cli_tools/compute_transformation/target/compute_transformation-0.1.0-SNAPSHOT.jar --source-dataset /SCRATCH-BIRD/users/skunne/matched_heart/pointclouds/heart.pcd_src_v30s150v30s150.csv --target-dataset /SCRATCH-BIRD/users/skunne/matched_heart/pointclouds/heart.pcd_dst_v30s150v30s150.csv --transformation-model RIGID >> /SCRATCH-BIRD/users/skunne/matched_heart/transforms/v30s150v30s150.txt
+
+    ../../scripts_python/distance_matched_pointclouds_csv.py /SCRATCH-BIRD/users/skunne/matched_heart/transforms/v30s150v30s150.txt /SCRATCH-BIRD/users/skunne/matched_heart/pointclouds/heart.pcd_src_v30s150v30s150.csv /SCRATCH-BIRD/users/skunne/matched_heart/pointclouds/heart.pcd_dst_v30s150v30s150.csv
+
+* First command `./computeallmatchings.sh` or `./test_params ...`: compute point-to-point matching between two pointclouds, and output two files with the points listed in matching order.
+* Second command `./computealltransforms.sh` or `java -jar .../compute_transformation-0.1.0-SNAPSHOT.jar ...`: compute geometric transformation between the two pointclouds and output one file with the 4x4 matrix representing the affine transformation in homogenous coordinates.
+* Third command `./computealldistances.sh` or `../scripts_python/distance_matched_pointclouds_csv.py ...`: compute average distance between the two registered pointclouds.
+
+Output files for the first two steps are in folders `/SCRATCH-BIRD/users/skunne/matched_heart/pointclouds` and `/SCRATCH-BIRD/users/skunne/matched_heart/transforms/`. Output file for the third step is `distances.txt`.
+
+
 
 To get the 3d plot of distance with respect to voxels,seed:
 
--- tests/params/make_cmd.py
+++ tests/params/make_cmd.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import sys    # argv, exit
+import os     # path.join
 from itertools import product
 
 def print_usage_and_exit(argv):
@@ -44,18 +45,20 @@ def main(argv):
         )
 
 # './test_params pointclouds/heart.meta pointclouds/heart.pcd 20 90 pointclouds/heart_rotatedpi4.pcd 20 90 pointclouds/heart.pcd 20 110 pointclouds/heart_rotatedpi4.pcd 20 110 pointclouds/heart.pcd 20 130 pointclouds/heart_rotatedpi4.pcd 20 130 pointclouds/heart.pcd 20 150 pointclouds/heart_rotatedpi4.pcd 20 150 pointclouds/heart.pcd 20 170 pointclouds/heart_rotatedpi4.pcd 20 170 pointclouds/heart.pcd 20 190 pointclouds/heart_rotatedpi4.pcd 20 190 pointclouds/heart.pcd 20 210 pointclouds/heart_rotatedpi4.pcd 20 210 pointclouds/heart.pcd 20 230 pointclouds/heart_rotatedpi4.pcd 20 230 pointclouds/heart.pcd 20 250 pointclouds/heart_rotatedpi4.pcd 20 250 pointclouds/heart.pcd 20 270 pointclouds/heart_rotatedpi4.pcd 20 270 pointclouds/heart.pcd 20 290 pointclouds/heart_rotatedpi4.pcd 20 290 pointclouds/heart.pcd 25 90 pointclouds/heart_rotatedpi4.pcd 25 90 pointclouds/heart.pcd 25 110 pointclouds/heart_rotatedpi4.pcd 25 110 pointclouds/heart.pcd 25 130 pointclouds/heart_rotatedpi4.pcd 25 130 pointclouds/heart.pcd 25 150 pointclouds/heart_rotatedpi4.pcd 25 150 pointclouds/heart.pcd 25 170 pointclouds/heart_rotatedpi4.pcd 25 170 pointclouds/heart.pcd 25 190 pointclouds/heart_rotatedpi4.pcd 25 190 pointclouds/heart.pcd 25 210 pointclouds/heart_rotatedpi4.pcd 25 210 pointclouds/heart.pcd 25 230 pointclouds/heart_rotatedpi4.pcd 25 230 pointclouds/heart.pcd 25 250 pointclouds/heart_rotatedpi4.pcd 25 250 pointclouds/heart.pcd 25 270 pointclouds/heart_rotatedpi4.pcd 25 270 pointclouds/heart.pcd 25 290 pointclouds/heart_rotatedpi4.pcd 25 290 pointclouds/heart.pcd 30 90 pointclouds/heart_rotatedpi4.pcd 30 90 pointclouds/heart.pcd 30 110 pointclouds/heart_rotatedpi4.pcd 30 110 pointclouds/heart.pcd 30 130 pointclouds/heart_rotatedpi4.pcd 30 130 pointclouds/heart.pcd 30 150 pointclouds/heart_rotatedpi4.pcd 30 150 pointclouds/heart.pcd 30 170 pointclouds/heart_rotatedpi4.pcd 30 170 pointclouds/heart.pcd 30 190 pointclouds/heart_rotatedpi4.pcd 30 190 pointclouds/heart.pcd 30 210 pointclouds/heart_rotatedpi4.pcd 30 210 pointclouds/heart.pcd 30 230 pointclouds/heart_rotatedpi4.pcd 30 230 pointclouds/heart.pcd 30 250 pointclouds/heart_rotatedpi4.pcd 30 250 pointclouds/heart.pcd 30 270 pointclouds/heart_rotatedpi4.pcd 30 270 pointclouds/heart.pcd 30 290 pointclouds/heart_rotatedpi4.pcd 30 290 pointclouds/heart.pcd 35 90 pointclouds/heart_rotatedpi4.pcd 35 90 pointclouds/heart.pcd 35 110 pointclouds/heart_rotatedpi4.pcd 35 110 pointclouds/heart.pcd 35 130 pointclouds/heart_rotatedpi4.pcd 35 130 pointclouds/heart.pcd 35 150 pointclouds/heart_rotatedpi4.pcd 35 150 pointclouds/heart.pcd 35 170 pointclouds/heart_rotatedpi4.pcd 35 170 pointclouds/heart.pcd 35 190 pointclouds/heart_rotatedpi4.pcd 35 190 pointclouds/heart.pcd 35 210 pointclouds/heart_rotatedpi4.pcd 35 210 pointclouds/heart.pcd 35 230 pointclouds/heart_rotatedpi4.pcd 35 230 pointclouds/heart.pcd 35 250 pointclouds/heart_rotatedpi4.pcd 35 250 pointclouds/heart.pcd 35 270 pointclouds/heart_rotatedpi4.pcd 35 270 pointclouds/heart.pcd 35 290 pointclouds/heart_rotatedpi4.pcd 35 290 pointclouds/heart.pcd 40 90 pointclouds/heart_rotatedpi4.pcd 40 90 pointclouds/heart.pcd 40 110 pointclouds/heart_rotatedpi4.pcd 40 110 pointclouds/heart.pcd 40 130 pointclouds/heart_rotatedpi4.pcd 40 130 pointclouds/heart.pcd 40 150 pointclouds/heart_rotatedpi4.pcd 40 150 pointclouds/heart.pcd 40 170 pointclouds/heart_rotatedpi4.pcd 40 170 pointclouds/heart.pcd 40 190 pointclouds/heart_rotatedpi4.pcd 40 190 pointclouds/heart.pcd 40 210 pointclouds/heart_rotatedpi4.pcd 40 210 pointclouds/heart.pcd 40 230 pointclouds/heart_rotatedpi4.pcd 40 230 pointclouds/heart.pcd 40 250 pointclouds/heart_rotatedpi4.pcd 40 250 pointclouds/heart.pcd 40 270 pointclouds/heart_rotatedpi4.pcd 40 270 pointclouds/heart.pcd 40 290 pointclouds/heart_rotatedpi4.pcd 40 290'
+    matched_pointclouds_folder = '/SCRATCH-BIRD/users/skunne/matched_heart/pointclouds/'
     with open('computealltransforms.sh', 'w') as outf_transforms:
         for v,s in product(voxels, seeds):
             print(
                 ' '.join([
                     computetransform_cmd(
-                        'matched_pointclouds/{}.pcd_src_v{}s{}v{}s{}.csv'.format(name, v,s,v,s),
-                        'matched_pointclouds/{}_rotatedpi4.pcd_dst_v{}s{}v{}s{}.csv'.format(name, v,s,v,s)
+                        os.path.join(matched_pointclouds_folder, '{}.pcd_src_v{}s{}v{}s{}.csv'.format(name, v,s,v,s)),
+                        os.path.join(matched_pointclouds_folder, '{}_rotatedpi4.pcd_dst_v{}s{}v{}s{}.csv'.format(name, v,s,v,s))
                     ),
                     '> transforms/v{}s{}v{}s{}.txt'.format(v,s,v,s)
                 ]),
                 file=outf_transforms)
 
+    transforms_folder = '/SCRATCH-BIRD/users/skunne/matched_heart/transforms/'
     with open('computealldistances.sh', 'w') as outf_distances:
         distancefilename = 'distances.txt'
         for v,s in product(voxels, seeds):
@@ -63,7 +66,7 @@ def main(argv):
             print('echo -n "{} " >> {}'.format(params, distancefilename), file=outf_distances)
             print(
                 ' '.join([
-                    '../../scripts_python/distance_matched_pointclouds_csv.py transforms/{}.txt matched_pointclouds/{}.pcd_src_{}.csv matched_pointclouds/{}_rotatedpi4.pcd_dst_{}.csv'.format(params, name, params, name, params),
+                    '../../scripts_python/distance_matched_pointclouds_csv.py {}/{}.txt {}/{}.pcd_src_{}.csv {}/{}_rotatedpi4.pcd_dst_{}.csv'.format(transforms_folder, params, matched_pointclouds_folder, name, params, matched_pointclouds_folder, name, params),
                     '>> {}'.format(distancefilename)
                 ]),
                 file=outf_distances
