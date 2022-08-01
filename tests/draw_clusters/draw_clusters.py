11e8189a60ae95f98202bf7b954997f985a3129d
Stephan KUNNE <skunne@login-01.compute.bird2.prive>
2022-08-01 11:24:57 +0200

    draw_clusters: add possibility to specify image output filename.

--- tests/draw_clusters/draw_clusters.py
+++ tests/draw_clusters/draw_clusters.py
@@ -19,10 +19,22 @@ def draw_pointcloud(pc):
     ax = fig.add_subplot(projection='3d')
     ax.scatter(X, Y, Z, c=L)
 
+def print_usage(cmd):
+    print('SYNOPSIS')
+    print()
+    print('{} <pc file> [<img file>]'.format(cmd))
+    print('      Display labeled pointcloud with one colour per label.')
+    print('      Store resulting image to <img file>; or to "Figure1.png".')
+
 def main(argv):
-    pc = read_pointcloud(argv[1])
+    if len(argv) > 1:
+        pc_filename = argv[1]
+        img_filename = argv[2] if len(argv) > 2 else 'Figure1.png'
+    else:
+        print_usage(argv[0])
+    pc = read_pointcloud(pc_filename)
     draw_pointcloud(pc)
-    plt.savefig('Figure1.png')
+    plt.savefig(img_filename)
     plt.show()
 
 if __name__ == '__main__':
