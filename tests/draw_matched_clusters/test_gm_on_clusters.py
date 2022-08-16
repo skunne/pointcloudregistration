9c1cecd96feb9d12382b16beb1b30477371255bb
Stephan KUNNE <skunne@login-01.compute.bird2.prive>
2022-08-16 17:35:52 +0200

    python part, not working

--- tests/draw_matched_clusters/test_gm_on_clusters.py
+++ tests/draw_matched_clusters/test_gm_on_clusters.py
@@ -3,31 +3,35 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import sys
+import numpy as np   # loadtxt
 
 def read_pointcloud(filename):
-    pc = []
-    with open(filename, 'r') as f:
-        for line in f:
-            xyz, label = line.strip('()\n').split(' - ')
-            x,y,z = xyz.split(',')
-            try:
-                x,y,z,label = map(int, (x,y,z,label))
-            except ValueError:
-                x,y,z = map(float, (x,y,z))
-                label = int(label)
-            pc.append((x,y,z,label))
+    pc = np.loadtxt(filename, delimiter=',', skiprows=1)
+    #L, X, Y, Z = array[:,0], array[:,1], array[:,2], array[:,3]
     return pc
 
-def draw_pointcloud(pc):
-    X,Y,Z,L = zip(*pc)
+def rate_points(pc_src, pc_dst):
+    green, red = [], []
+    target = {(x,y,z): l for l,x,y,z in pc_dst}
+    for (l,x,y,z) in pc_src:
+        if l == target.get((x+10, y+10, z+10), -1):
+            green.append((x,y,z))
+        else:
+            red.append((x,y,z))
+    return green, [], red
+
+def draw_pointcloud(green, orange, red):
     #cmap = plt.cm.get_cmap('hsv', len(set(L)))
-    cmap = mcolors.ListedColormap(mcolors.TABLEAU_COLORS.values())
+    # cmap = mcolors.ListedColormap(mcolors.TABLEAU_COLORS.values())
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    ax.scatter(X, Y, Z, c=L, cmap=cmap)
+    #for pc, colour in zip((green, orange, red), ('green', 'orange', 'red')):
+    for pc, colour in zip((green, red), ('green', 'red')):
+        X,Y,Z = zip(*pc)
+        ax.scatter(X, Y, Z, c=colour)
 
 def print_usage(cmd):
     print('SYNOPSIS')
@@ -37,19 +41,22 @@ def print_usage(cmd):
     print('      output to img file')
 
 def main(argv):
-    if len(argv) > 3:
+    if len(argv) >= 3:
         pc_src_filename = argv[1]
         pc_dst_filename = argv[2]
-        if len(argv) > 4:
+        if len(argv) >= 4:
             output_filename = argv[3]
         else:
             output_filename = 'out.png'
     else:
         print_usage(argv[0])
+        sys.exit(-1)
     pc_src = read_pointcloud(pc_src_filename)
     pc_dst = read_pointcloud(pc_dst_filename)
     green, orange, red = rate_points(pc_src, pc_dst)
     draw_pointcloud(green, orange, red)
+    plt.savefig(output_filename)
+    plt.show()
 
 if __name__ == '__main__':
     main(sys.argv)
