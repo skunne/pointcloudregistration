5e78936d456baaedcec41fe9b57c192c7e3962c0
Stephan KUNNE <skunne@login-01.compute.bird2.prive>
2022-08-16 17:03:10 +0200

    c++ part working

--- tests/draw_matched_clusters/main_test.cpp
+++ tests/draw_matched_clusters/main_test.cpp
@@ -55,7 +55,7 @@ void test_writeresult(ProcessedPointCloud const &ppc_source, ProcessedPointCloud
 
       for (auto const &p : *(clusters_dest.at(j)->voxels_))
       {
-        src_out << i << ',' << p.x << ',' << p.y << ',' << p.z << std::endl;
+        dst_out << i << ',' << p.x << ',' << p.y << ',' << p.z << std::endl;
       }
       // src_out << i << ',' << std::get<0>(pc_source[i])
       //              << ',' << std::get<1>(pc_source[i])
@@ -71,14 +71,15 @@ void test_writeresult(ProcessedPointCloud const &ppc_source, ProcessedPointCloud
 int test_printUsage(char const *cmd)
 {
   std::cout << "SYNOPSIS" << std::endl << std::endl;
-  std::cout << cmd << " <meta> <pc> <out>" << std::endl;
-  std::cout << "    Apply supervoxel to pointcloud <pc> with params from .meta file <meta>." << std::endl;
-  std::cout << "    <meta> must be the name of a metadata file. The point cloud filename given inside <meta> is ignored; only the parameters are used." << std::endl;
+  std::cout << cmd << " <meta> <pc src> <pc dst> <out>" << std::endl;
+  std::cout << "    Run supervoxel clustering and graph-matching on two pointclouds." << std::endl;
   std::cout << "OUTPUT" << std::endl;
-  std::cout << "    Results are printed to text file <out>:." << std::endl;
-  std::cout << "    one point per line, with labels. Two points have same label iff they belong to same supervoxel." << std::endl;
+  std::cout << "    Results are printed to two text files src_<out> and dst_<out>." << std::endl;
+  std::cout << "    One point per line, with labels." << std::endl;
+  std::cout << "    Two points in same file have same label iff they belong to same supervoxel." << std::endl;
+  std::cout << "    Two labels in different files are equal iff those clusters are matched by graph-matching." << std::endl;
   std::cout << "EXAMPLE USE" << std::endl;
-  std::cout << cmd << " heart.meta heart.pcd" << std::endl;
+  std::cout << cmd << " heart.meta heart.pcd heart_rotated.pcd out.txt" << std::endl;
   return 1;
 }
 
@@ -96,16 +97,17 @@ void test_printVoxels(ProcessedPointCloud const &ppc, std::string const &filenam
 
 int main(int argc, char ** argv)
 {
-  if (argc < 4)
+  if (argc < 5)
     return test_printUsage(argv[0]);
 
   std::string out_folder = "/SCRATCH-BIRD/users/skunne/";
-  std::string out_filename(argv[3]);
+  std::string out_filename(argv[4]);
 
   Params params(argv[1]);
   params.filename = argv[2];
   ProcessedPointCloud ppc_source(params);
-  ProcessedPointCloud ppc_dest(argv[2]);
+  params.filename = argv[3];
+  ProcessedPointCloud ppc_dest(params);
 
   if (ppc_source.error() || ppc_dest.error())
     return 1;
@@ -125,15 +127,15 @@ int main(int argc, char ** argv)
   VertexSimilarityMatrix vsim_mat(ppc_source.esf_descriptors, ppc_dest.esf_descriptors);
   EdgeSimilarityMatrix esim_mat(ppc_source.edge_descriptors, ppc_dest.edge_descriptors);
 
-  printMatrixToFile("output/esf_similarity_matrix", vsim_mat.m);
-  printMatrixToFile("output/edge_similarity_matrix", esim_mat.m);
+  printMatrixToFile("esf_similarity_matrix", vsim_mat.m);
+  printMatrixToFile("edge_similarity_matrix", esim_mat.m);
 
   GraphMatchingFrankwolfe gm(&vsim_mat.m, &esim_mat, &ppc_source.adjacency_matrix, &ppc_dest.adjacency_matrix);
   // gm.frankWolfe(0.0, &permutation_matrix, &permutation_matrix);
   // GraphMatchingNonlin gm(&vsim_mat.m, &esim_mat, &ppc_source.adjacency_matrix, &ppc_dest.adjacency_matrix);
   gm.run();
 
-  test_writeresult(ppc_source, ppc_dest, gm.matching, "src_" + out_filename, "src_" + out_filename);
+  test_writeresult(ppc_source, ppc_dest, gm.matching, "src_" + out_filename, "dst_" + out_filename);
 
 
   return (0);
--- tests/draw_matched_clusters/test_gm_on_clusters.py
+++ tests/draw_matched_clusters/test_gm_on_clusters.py
@@ -18,30 +18,38 @@ def read_pointcloud(filename):
             pc.append((x,y,z,label))
     return pc
 
-# def draw_pointcloud(pc):
-#     X,Y,Z,L = zip(*pc)
-#     #cmap = plt.cm.get_cmap('hsv', len(set(L)))
-#     cmap = mcolors.ListedColormap(mcolors.TABLEAU_COLORS.values())
-#     fig = plt.figure()
-#     ax = fig.add_subplot(projection='3d')
-#     ax.set_xlabel('x')
-#     ax.set_ylabel('y')
-#     ax.set_zlabel('z')
-#     ax.scatter(X, Y, Z, c=L, cmap=cmap)
+def draw_pointcloud(pc):
+    X,Y,Z,L = zip(*pc)
+    #cmap = plt.cm.get_cmap('hsv', len(set(L)))
+    cmap = mcolors.ListedColormap(mcolors.TABLEAU_COLORS.values())
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.scatter(X, Y, Z, c=L, cmap=cmap)
 
 def print_usage(cmd):
     print('SYNOPSIS')
     print()
-    print('{} <pc file 1> <pc file 2> [<>]'.format(cmd))
+    print('{} <pc file 1> <pc file 2> [<img file>]'.format(cmd))
     print('      ...')
-    print('      ...')
+    print('      output to img file')
 
 def main(argv):
-    if len(argv) > 1:
-        pc_src = argv[1]
+    if len(argv) > 3:
+        pc_src_filename = argv[1]
+        pc_dst_filename = argv[2]
+        if len(argv) > 4:
+            output_filename = argv[3]
+        else:
+            output_filename = 'out.png'
     else:
         print_usage(argv[0])
-    pc = read_pointcloud(pc_src)
+    pc_src = read_pointcloud(pc_src_filename)
+    pc_dst = read_pointcloud(pc_dst_filename)
+    green, orange, red = rate_points(pc_src, pc_dst)
+    draw_pointcloud(green, orange, red)
 
 if __name__ == '__main__':
     main(sys.argv)
