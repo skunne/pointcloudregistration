352dcadb8025ad866a46f29e6b909493852329ed
Stephan KUNNE <skunne@login-01.compute.bird2.prive>
2022-08-19 14:10:01 +0200

    figure heart identity

--- tests/draw_matched_clusters/script.sh
+++ tests/draw_matched_clusters/script.sh
@@ -1,6 +1,6 @@
 for f in heart #nuclei valve
 do
-    for r in translated10 rotatedpi8 rotatedpi4
+    for r in id translated10 rotatedpi8 rotatedpi4
     do
         ./print_voxels ../params/"$f"/pointclouds/"$f".meta ../params/"$f"/pointclouds/"$f".pcd ../params/"$f"/pointclouds/"$f"_"$r".pcd out.txt
         python3 test_gm_on_clusters.py src_out.txt dst_out.txt mat.txt transforms/"$r".csv Figure_"$f"_"$r".png
