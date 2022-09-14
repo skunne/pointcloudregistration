db2f0566822aad926800c514fae45621def8dc97
skunne <stephan.kunne@gmail.com>
2022-09-14 12:17:12 +0200

    fix bug rmse

--- scripts_python/rmse_matched_pointclouds_csv.py
+++ scripts_python/rmse_matched_pointclouds_csv.py
@@ -43,7 +43,7 @@ def main(argv):
     src_filename, dst_filename, transform_matrix_filename = get_args(argv)
     pc_src = np.loadtxt(src_filename, delimiter=',', skiprows=1, usecols=(1,2,3))
     pc_dst = np.loadtxt(dst_filename, delimiter=',', skiprows=1, usecols=(1,2,3))
-    transform_matrix =  np.loadtxt(transform_matrix_filename, delimiter=',') if transform_matrix_filename else None
+    transform_matrix =  np.loadtxt(transform_matrix_filename) if transform_matrix_filename else None
     rmse = compute_rmse(pc_src, pc_dst, transform_matrix)
     print('{} {} {} --> RMSE = {}'.format(transform_matrix_filename, src_filename, dst_filename, rmse))
 
